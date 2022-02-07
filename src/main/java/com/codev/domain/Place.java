51f28383dddbcedb218ea5b8639c5ced3ceac7e9
aubresson <aurelien.bress43@gmail.com>
2022-02-07 17:01:00 +0100

    change database schema

--- src/main/java/com/codev/domain/Place.java
+++ src/main/java/com/codev/domain/Place.java
@@ -14,7 +14,6 @@ public class Place {
 
     @Id
     @Column(name = "id", nullable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public int getId() {
         return id;
     }
