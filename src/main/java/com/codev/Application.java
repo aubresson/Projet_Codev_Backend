1377ee99310a8b0dc397f988cb0f46dd91b986a4
aubresson <aurelien.bress43@gmail.com>
2022-02-08 09:11:33 +0100

    optimize code

--- src/main/java/com/codev/Application.java
+++ src/main/java/com/codev/Application.java
@@ -4,9 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class Application  extends SpringBootServletInitializer {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
 
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
